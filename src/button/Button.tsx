7b281423771f06f05a30efb4d26275dae4044b8f
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-02-04 19:23:48 +0300

    first commit

---
+++ src/button/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Button = () => {
+    return (
+        <div>
+            <button>MyYTChannel-1</button>
+            <button>MyYTChannel-2</button>
+        </div>
+    );
+};
---
+++ src/mapMethod/MapMetod.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// const topCars = [
+//     {manufacturer:'BMW', model:'m5cs'},
+//     {manufacturer:'Mercedes', model:'e63s'},
+//     {manufacturer:'Audi', model:'rs6'}
+// ]
+
+export const MapMetod = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
