0e51e49de686d8fafcb141f12f1c91d955757879
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-02-28 22:35:21 +0300

    Second micro-task (map method practice)

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
 
+type CarsProps = {
+    manufacturer: string
+    model: string
+}
+
+type StudentsProps = {
+    id: number
+    name: string
+    age: number
+}
+
 type BodyProps = {
     title: string
+    students: StudentsProps[]
+    cars: CarsProps[]
 }
-export const Body = ({title}: BodyProps) => {
+export const Body = ({title, students, cars}: BodyProps) => {
     return (
-        <body>
+        <div>
             <h2>{title}</h2>
-            Body
-        </body>
+            <p>Students list</p>
+            <ul>
+                {
+                    students.map((student) => {
+                        return (
+                            <li key={student.id}>
+                                <div>Student name: {student.name}</div>
+                                <div>Student age: {student.age}</div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            <p>Top cars list</p>
+            <table>
+                {
+                    cars.map((car, index) => {
+                        return (
+                            <tr key={index}>
+                                <th>{car.manufacturer}</th>
+                                <th>{car.model}</th>
+                            </tr>
+                        )
+                    })
+                }
+            </table>
+        </div>
     );
 };
