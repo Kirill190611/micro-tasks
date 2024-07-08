3c7eacd23f01a7e74b6e725c82a0eeec0caf8d04
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-07-08 22:25:56 +0300

    8th micro-task (practice with react-router-dom v6.24)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from "./components/Site.module.css";
-import {Adidas} from "./components/pages/Adidas";
-import {Puma} from "./components/pages/Puma";
-import {Abibas} from "./components/pages/Abibas";
-import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
-import {Error404} from "./components/pages/Error404";
-import styled from 'styled-components';
+import {
+    NavLink,
+    Outlet
+} from 'react-router-dom';
 import {S} from './components/pages/_styles'
-import {Model} from "./components/pages/Model";
-import {Prices} from "./components/pages/Prices";
-
-const PATH = {
-    PAGE1: '/adidas',
-    PAGE2: '/puma',
-    PAGE3: '/abibas',
-    PRICES: '/prices',
-} as const
+import {PATH} from "./routes/router";
 
 function App() {
     return (
@@ -23,28 +13,28 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <S.NavWrapper><NavLink to={PATH.PAGE1}>Adidas</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE2}>Puma</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE3}>Abibas</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.ADIDAS}>Adidas</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PUMA}>Puma</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.ABIBAS}>Abibas</NavLink></S.NavWrapper>
                     <S.NavWrapper><NavLink to={PATH.PRICES}>Цены для оптовиков</NavLink></S.NavWrapper>
                 </div>
                 <div className={styles.content}>
-                    <Routes>
+                    <Outlet/>
+                    {/*<Routes>
                         <Route path={'/'} element={<Navigate to={PATH.PAGE1}/>}/>
 
                         <Route path={PATH.PAGE1} element={<Adidas/>}/>
                         <Route path={PATH.PAGE2} element={<Puma/>}/>
                         <Route path={PATH.PAGE3} element={<Abibas/>}/>
                         <Route path={PATH.PRICES} element={<Prices/>}/>
-                        {/*<Route path={'/adidas/:id'} element={<Model/>}/>*/}
+                        <Route path={'/adidas/:id'} element={<Model/>}/>
                         <Route path={'/:model/:id'} element={<Model/>}/>
 
                         <Route path={'/*'} element={<Error404/>}/>
 
-                        {/*<Route path={'/page/error'} element={<Error404/>}/>*/}
-                        {/*<Route path={'/*'} element={<Navigate to={'/page/error'}/>}/>*/}
-                    </Routes>
-
+                        <Route path={'/page/error'} element={<Error404/>}/>
+                        <Route path={'/*'} element={<Navigate to={'/page/error'}/>}/>
+                    </Routes>*/}
                 </div>
             </div>
             <div className={styles.footer}>abibas 2023</div>
